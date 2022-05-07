b16d8f851ccd62ff5f763ffcabf3601d7df8d510
codewithkevin <oseikelvin709@gmail.com>
2022-05-07 23:44:17 +0000

    update

--- js/main.js
+++ js/main.js
@@ -25,34 +25,7 @@ const message = document.querySelector('#message');
 const seconds = document.querySelector('#seconds');
 const highscoreDisplay = document.querySelector('#highscore');
 
-const words = [
-  'hat',
-  'river',
-  'lucky',
-  'statue',
-  'generate',
-  'stubborn',
-  'cocktail',
-  'runaway',
-  'joke',
-  'developer',
-  'establishment',
-  'hero',
-  'javascript',
-  'nutrition',
-  'revolver',
-  'echo',
-  'siblings',
-  'investigate',
-  'horrendous',
-  'symptom',
-  'laughter',
-  'magic',
-  'master',
-  'space',
-  'definition'
-];
-
+const words = "https://random-word-api.herokuapp.com/word?number=100";
 // Initialize Game
 function init() {
   // Show number of seconds in UI
@@ -110,10 +83,12 @@ function matchWords() {
 
 // Pick & show random word
 function showWord(words) {
-  // Generate random array index
-  const randIndex = Math.floor(Math.random() * words.length);
-  // Output random word
-  currentWord.innerHTML = words[randIndex];
+  fetch(words)
+    .then(response => response.json())
+    .then(data => {
+      const randomIndex = Math.floor(Math.random() * data.length);
+      currentWord.innerHTML = data[randomIndex];
+    });
 }
 
 // Countdown timer
