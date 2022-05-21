539e4c39a62e8290b8b4610e7ef01117bd204d61
codewithkevin <oseikelvin709@gmail.com>
2022-05-21 02:24:32 +0000

    update

--- js/main.js
+++ js/main.js
@@ -4,93 +4,8 @@ const mediumBtn = document.querySelector('#mediumBtn');
 const hardBtn = document.querySelector('#hardBtn');
 
 
-// Event Listeners
-easyBtn.addEventListener('click', easyBtnClick);
 
-function easyBtnClick() {
 
-  //DOM Selectors
-  const currentWord = document.querySelector('#current-word');
-  const wordInput = document.querySelector('#word-input');
-  const scoreDisplay = document.querySelector('#score');
-  const timeDisplay = document.querySelector('#time');
-  const message = document.querySelector('#message');
-  const seconds = document.querySelector('#seconds');
-  const highscoreDisplay = document.querySelector('#highscore');
-
-  const words = "https://random-word-api.herokuapp.com/word?number=100";
-
-  async function getWords() {
-    const response = await fetch(words);
-    const data = await response.json();
-    currentWord.innerHTML = data[0];
-
-    // Initialize Game
-    const current_level = 10;
-    let time = current_level;
-    let score = 0;
-    let isPlaying = true;
-
-    function init() {
-      // Show number of seconds in UI
-      seconds.innerHTML = current_level;
-      // Load word from array
-      showWord(words);
-      // Start matching on word input
-      wordInput.addEventListener('input', startMatch);
-      // Call countdown every second
-      setInterval(countdown, 1000);
-      // Check game status
-      setInterval(checkStatus, 50);
-    }
-
-    //Start match
-    function startMatch() {
-      if (matchWords()) {
-        isPlaying = true;
-        time = currentLevel + 1;
-        showWord(words);
-        wordInput.value = '';
-        score++;
-      }
-      
-      // Highscore based on score value for Session Storage
-      if (typeof sessionStorage['highscore'] === 'undefined' || score > sessionStorage['highscore']) {
-        sessionStorage['highscore'] = score;
-      } else {
-        sessionStorage['highscore'] = sessionStorage['highscore'];
-      }
-
-      // Prevent display of High Score: -1
-      if (sessionStorage['highscore'] >= 0) {
-      highscoreDisplay.innerHTML = sessionStorage['highscore'];
-      }
-
-      // If score is -1, display 0
-      if (score === -1) {
-        scoreDisplay.innerHTML = 0;
-      } else {
-        scoreDisplay.innerHTML = score;
-      }
-    }
-
-    // Match currentWord to wordInput
-    function matchWords() {
-      if (wordInput.value === currentWord.innerHTML) {
-        message.innerHTML = 'Correct!!!';
-        return true;
-      } else {
-        message.innerHTML = '';
-        return false;
-      }
-    }
-    
-    
-  }
-  getWords();
-  
-
-}
 
 
 
@@ -213,5 +128,5 @@ function easy() {
 
 }
 
-easy()
+easy();
 
