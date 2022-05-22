4738a7eef60359a623b332f7490b0c8ff858d3b3
codewithkevin <oseikelvin709@gmail.com>
2022-05-22 11:59:42 +0000

    update

---
+++ main.py
@@ -0,0 +1,11 @@
+"""List"""
+names = ["kelvin", "osei", "Kelvin", "Hello", "Exams", "God"]
+slice = names[2:]
+change = names[1] = "Welcom To 201"
+call = names[-1]
+add = names+["Lord is God"]
+delting = len(names)
+pop = names.pop(0)
+insert = names.insert(1, "lvin")
+
+
