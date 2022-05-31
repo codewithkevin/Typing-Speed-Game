2c7b233de8d13e2b3c3126912e3b3a1d4493eb46
codewithkevin <oseikelvin709@gmail.com>
2022-05-31 13:04:33 +0000

    update

--- js/main.js
+++ js/main.js
@@ -26,21 +26,57 @@ function easyBtnClick() {
 
   let time = currentLevel;
   let score = 0;
-  let isPlaying = true;
+  let isPlaying;
 
-  //Get random word from array
+  //Word APi
   const words = "https://random-word-api.herokuapp.com/word?number=100"
-  fetch(words)
-  .then(response => response.json())
-  .then(data => {
-    let word = data[Math.floor(Math.random() * data.length)];
-    word = word.toUpperCase();
-    currentWord.innerHTML = word;
-  });
+  
 
-  //Initialize game
-  seconds.innerHTML = currentLevel;
-  scoreDisplay.innerHTML = score;
-  timeDisplay.innerHTML = time;
-  message.innerHTML = "";
+  
+  //INITIALIZE GAME
+  function init(){
+    //Show seconds
+    seconds.innerHTML = currentLevel;
+    //Load word from array
+    showWord(words);
+    //Start matching on word input
+    wordInput.addEventListener('input', startMatch);
+    //Call countdown every second
+    setInterval(countdown, 1000);
+    //Check game status
+    setInterval(checkStatus, 50);
+  }
+
+  //Start match
+  function startMatch(){
+   if(matchWords()){
+     isPlaying = true;
+     time = currentLevel + 1;
+     showWord(words);
+     wordInput.value = '';
+     score++;
+   }
+  }
+
+  //SHow word
+  function showWord(words){
+    fetch(words)
+    .then(response => response.json())
+    .then(data => {
+      const randomWord = data[Math.floor(Math.random() * data.length)];
+      currentWord.innerHTML = randomWord;
+    });
+  }
+
+  //HIGHSCORE
+  if(typeof sessionStorage['highscore'] == 'undefined' || score > sessionStorage['highscore']){
+    sessionStorage['highscore'] = score;
+  }
+  else{
+    sessionStorage['highscore'] = sessionStorage['highscore'];
+  }
+
+
+  init();
+  
 }
